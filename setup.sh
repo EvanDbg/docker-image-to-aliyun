e61c1a948519f6f81ce34588ed1a93832cf27afd
EvanDbg <yangf1023@icloud.com>
2020-06-23 07:05:08 +0800

    add arm64e support

--- setup.sh
+++ setup.sh
@@ -49,3 +49,14 @@ apt-get purge --yes --autoremove $BUILD_DEPS
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm $0
+
+curl -LO https://github.com/sbingner/llvm-project/releases/download/v10.0.0-1/linux-ios-arm64e-clang-toolchain.tar.lzma
+TMP=$(mktemp -d)
+tar --lzma -xvf linux-ios-arm64e-clang-toolchain.tar.lzma -C $TMP
+pushd $TMP/ios-arm64e-clang-toolchain/bin
+find * ! -name clang-10 -and ! -name ldid -and ! -name ld64 -exec mv {} arm64-apple-darwin14-{} \;
+find * -xtype l -exec sh -c "readlink {} | xargs -I{LINK} ln -f -s arm64-apple-darwin14-{LINK} {}" \;
+popd
+mkdir -p $THEOS/toolchain/linux/iphone
+rsync -a $TMP/ios-arm64e-clang-toolchain/* $THEOS/toolchain/linux/iphone/
+rm -rf $TMP linux-ios-arm64e-clang-toolchain.tar.lzma
